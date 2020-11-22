dae3a2dd8e707bee0a0d53369e47481281ba73dd
marcelohmdias <marcelohmdias@gmail.com>
2020-11-22 19:14:12 -0300

    chore: add new base state and events for change

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,14 +1,54 @@
-import { Product, State } from './state'
+import { produce } from 'immer'
 
-export type ReducerAction = {
-  type: 'LOAD_PRODUCTS'
-  payload: Array<Product>
-}
+import { Cart, Product, State } from './state'
+
+export type ReducerAction =
+  | {
+      type: 'ADD_TO_CART'
+      payload: Cart
+    }
+  | {
+      type: 'CART_LOADING'
+      payload: boolean
+    }
+  | {
+      type: 'LOAD_PRODUCTS'
+      payload: Array<Product>
+    }
+  | {
+      type: 'FILTER_PRODUCTS'
+      payload: string
+    }
 
 export function reducer(state: State, action: ReducerAction) {
   switch (action.type) {
+    case 'ADD_TO_CART':
+      return produce(state, (draft) => {
+        draft.cart = action.payload
+      })
+
+    case 'CART_LOADING':
+      return produce(state, (draft) => {
+        draft.cartLoading = action.payload
+      })
+
+    case 'FILTER_PRODUCTS':
+      return produce(state, (draft) => {
+        const str = action.payload?.trim().toLowerCase()
+
+        draft.products = draft.products.map((item) => {
+          item.visible = str
+            ? item.description.toLowerCase().includes(str)
+            : true
+          return item
+        })
+      })
+
     case 'LOAD_PRODUCTS':
-      return { ...state, products: action.payload }
+      return produce(state, (draft) => {
+        draft.products = action.payload
+      })
+
     default:
       return state
   }
--- src/store/state.ts
+++ src/store/state.ts
@@ -1,17 +1,28 @@
-export interface Product {
-  id: string
-  description: string
+export type Product = {
   color: string
-  price: string
   currency: string
-  thumbnailURL: string
+  description: string
+  id: string
   maxresURL: string
+  price: string
+  thumbnailURL: string
+  visible: boolean
+}
+
+export type Cart = {
+  product: Product
+  quantity: string
+  size: string
+  total: number
 }
 
 export interface State {
   products: Array<Product>
+  cart?: Cart
+  cartLoading: boolean
 }
 
 export const initialState: State = {
-  products: []
+  products: [],
+  cartLoading: false
 }
