41524b4b9d4017338c5366206a7feefd8be323dd
marcelohmdias <marcelohmdias@gmail.com>
2020-11-29 19:53:47 -0300

    refactor: extend component to multiple titles

--- src/components/atoms/title/Title.tsx
+++ src/components/atoms/title/Title.tsx
@@ -1,9 +1,17 @@
+import { StyledComponent } from 'styled-components'
+
+import { JSXProps } from '../../../types'
 import * as S from './styles'
 
+type Tag = 'h1' | 'h2' | 'h3'
+
 type TitleProps = {
-  title: string
+  tag: Tag
+  text: string
 }
 
-export default function Title({ title }: TitleProps) {
-  return <S.Title>{title}</S.Title>
+export default function Title({ tag, text, className }: JSXProps<TitleProps>) {
+  const h = tag.toUpperCase()
+  const Header = (S as Record<string, StyledComponent<Tag, any, {}, never>>)[h]
+  return <Header className={className}>{text}</Header>
 }
--- src/components/atoms/title/styles.ts
+++ src/components/atoms/title/styles.ts
@@ -1,11 +1,28 @@
 import styled from 'styled-components'
 
-export const Title = styled.h1`
-  color: #000000;
+export const H1 = styled.h1`
+  color: #000;
   font-family: Arial;
   font-size: 1.625em;
   font-style: normal;
   font-weight: normal;
   line-height: 30px;
-  text-align: center;
+`
+
+export const H2 = styled.h2`
+  color: #000;
+  display: inline-block;
+  font-size: 1.375em;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 30px;
+  margin-bottom: 1em;
+`
+
+export const H3 = styled.h3`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.125em;
+  line-height: 16px;
+  margin-bottom: 0.25em;
 `
